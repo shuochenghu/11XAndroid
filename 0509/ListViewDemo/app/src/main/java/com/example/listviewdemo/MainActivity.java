049ec45aafe3112e7a6e80fe6fa7d254fd415273
chuckhu <shuochenghu@gmail.com>
2024-05-09 18:25:32 +0800

    113-05-09

---
+++ 0509/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+    private String[] cities;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cities = getResources().getStringArray(R.array.cities);
+        lv = (ListView) findViewById(R.id.listview);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+                output.setText("你是住在: " + cities[position]);
+            }
+        });
+    }
+}
